b1d1b19a6f0ceb749ef951b10776fb290845e7ea
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-01-07 06:54:16 -0300

    Api placeholder

--- servers/api.py
+++ servers/api.py
@@ -1,6 +1,8 @@
 from space import web
 import logging
 import pyding
+import json
+
 
 logging.basicConfig(level=logging.INFO)
 print(":)")
@@ -9,4 +11,8 @@ print(":)")
 @pyding.on("http_request")
 def api(event, request, connection, address):
     if "Host" in request.headers and request.headers["Host"] == "api.jaobernardi.space":
-        return web.response(200, 'No content', {'Server': 'jaobernardi/backend'}, b"")
+        output = {"status": 200, "message": "OK"}
+
+
+        parsed_output = json.dumps(output)
+        return web.response(200, 'No content', {'Server': 'jaobernardi/backend'}, parsed_output.encode("utf-8"))
