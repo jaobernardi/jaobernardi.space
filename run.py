ccb1de54ae4c9e075d23858d9a637a10601ec1f1
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-01-07 05:21:00 -0300

    Add Errors

--- run.py
+++ run.py
@@ -9,16 +9,18 @@ server_modules = []
 
 logging.basicConfig(level=logging.INFO)
 
-@pyding.on("http_request", priority=10)
+@pyding.on("http_request", priority=float("-inf"))
 def fallback_server(event, request, connection, address):
-    file = open("html_test.htm", "rb")
+    file = open("web/index.htm", "rb")
     contents = file.read()
     try:
         parsed = web.eval_document(contents, {"request": request, "address": address}).encode("utf-8")
         return web.response(200, 'OK', {'Server': 'jaobernardi/backend'}, parsed)
     except:
         logging.error("Failed to meet demands.")
-        return web.response(500, 'Internal Server Error', {'Server': 'jaobernardi/backend'}, b"")
+        file = open("web/error.htm", "rb")
+        parsed = web.eval_document(contents, {"request": request, "address": address, "code": 500, "text": "Internal Server Error"}).encode("utf-8")
+        return web.response(500, 'Internal Server Error', {'Server': 'jaobernardi/backend'}, parsed)
 
 
 def load_servers():
--- servers/api.py
+++ servers/api.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO)
 print(":)")
 
 
-@pyding.on("http_request", priority=1000)
+@pyding.on("http_request")
 def api(event, request, connection, address):
     if "Host" in request.headers and request.headers["Host"] == "api.jaobernardi.space":
         return web.response(200, 'No content', {'Server': 'jaobernardi/backend'}, b"")
