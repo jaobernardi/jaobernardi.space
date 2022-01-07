492c950491f7e11c7aa0c3efbf84135e8a278049
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-01-07 05:23:00 -0300

    Add Errors

--- run.py
+++ run.py
@@ -19,6 +19,7 @@ def fallback_server(event, request, connection, address):
     except:
         logging.error("Failed to meet demands.")
         file = open("web/error.html", "rb")
+        contents = file.read()
         parsed = web.eval_document(contents, {"request": request, "address": address, "code": 500, "text": "Internal Server Error"}).encode("utf-8")
         return web.response(500, 'Internal Server Error', {'Server': 'jaobernardi/backend'}, parsed)
 
