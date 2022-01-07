5805a1098ed83ee182858e43391bef9326546154
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-01-07 04:57:40 -0300

    fix event call

--- servers/api.py
+++ servers/api.py
@@ -9,5 +9,4 @@ print(":)")
 @pyding.on("http_request", priority=1000)
 def api(event, request, connection, address):
     if "Host" in request.headers and request.headers["Host"] == "api.jaobernardi.space":
-        event.cancel()
-        return web.response(201, 'No content', {'Server': 'jaobernardi/backend'}, b"")
+        return web.response(200, 'No content', {'Server': 'jaobernardi/backend'}, b"")
