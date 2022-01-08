95dba6fce02018b2cccc9e94045d65be66744716
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-01-08 01:38:36 -0300

    Implement fallback server

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from space import web
+from space import web, Config
 import logging
 import pyding
 import os
@@ -9,19 +9,50 @@ server_modules = []
 
 logging.basicConfig(level=logging.INFO)
 
+config = Config()
+
+
 @pyding.on("http_request", priority=float("-inf"))
 def fallback_server(event, request, connection, address):
-    file = open("web/index.htm", "rb")
-    contents = file.read()
-    try:
-        parsed = web.eval_document(contents, {"request": request, "address": address}).encode("utf-8")
-        return web.response(200, 'OK', {'Server': 'jaobernardi/backend'}, parsed)
-    except:
-        logging.error("Failed to meet demands.")
-        file = open("web/error.html", "rb")
-        contents = file.read()
-        parsed = web.eval_document(contents, {"request": request, "address": address, "code": 500, "text": "Internal Server Error"}).encode("utf-8")
-        return web.response(500, 'Internal Server Error', {'Server': 'jaobernardi/backend'}, parsed)
+    error_file = open("web/error.html", "rb")
+    error_contents = error_file.read()    
+
+    if "Host" in request.headers and request.headers["Host"] == config.scopes["home"]:
+        default_headers = {"Server": "jspaces/1.0"}
+        path = os.path.join("web_assets/", request.path.removeprefix("/"))
+
+        if ".." in path:
+            error_parsed = web.eval_document(error_contents, {"request": request, "address": address, "code": 403, "text": "Unauthorized"}).encode("utf-8")
+            return web.response(403, 'Unauthorized', default_headers, error_parsed)
+
+        if os.path.exists(path):
+            # detect file
+            if os.path.isfile(path):
+                filename = path
+            elif os.path.exists(os.path.join(path, 'index.html')):
+                filename = os.path.join(path, "index.html")
+            else:
+                error_parsed = web.eval_document(error_contents, {"request": request, "address": address, "code": 404, "text": "Not Found"}).encode("utf-8")
+                return web.response(404, 'Not Found', default_headers, error_parsed)
+        else:
+            # not found response
+            error_parsed = web.eval_document(error_contents, {"request": request, "address": address, "code": 404, "text": "Not Found"}).encode("utf-8")
+            return web.response(404, 'Not Found', default_headers, error_parsed)
+        
+        data = open(filename, 'rb').read()
+        prefix = os.path.basename(filename).split(".")[-1]
+        if prefix in config.mime_types.mime_types:
+            default_headers['Content-Type'] = config.mime_types[prefix]
+        data = web.eval_document(data, {"request": request, "address": address}).encode("utf-8")
+        
+        return web.response(
+            200,
+            'OK', 
+            default_headers|{
+                'Content-Length': len(data),
+            },
+            data
+        )
 
 
 def load_servers():
