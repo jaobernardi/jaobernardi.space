5ce6e4df80607c96c898241fc6dc85dd287e3354
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-02-06 18:14:30 -0300

    Start rewrite

--- space/__init__.py
+++
@@ -1,3 +0,0 @@
-from .config import Node, Config
-from .web import Server, Request, eval_document, PythonTag, response
-from .database import *
--- space/database.py
+++
@@ -1,9 +0,0 @@
-# database.py * This file handles all calls to the database.
-from datetime import datetime
-import sqlite3
-
-from .config import Config
-import json
-
-# Currently i'll work with a local database 
-config = Config()
