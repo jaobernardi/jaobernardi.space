dd9f004f8347bfb88a5a49efe1fe24162021cf63
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-02-20 00:15:26 -0300

    Add main

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-# Placeholder main file
+from lib import web
+import pyding
+
+
+# Handle http requests
+@pyding.on("http_request")
+def responder(event, request):
+    # Always return 200 OK
+    return web.Response(200, "OK", data=request.data)
+
+
 def main():
-    return
+    # Setup server
+    server = web.Server("127.0.0.1", 1220)
+    server.spin_up()
 
 
 if __name__ == "__main__":
     main()
+
