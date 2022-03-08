071e2be9c08e63bd8035045f7958b3d80658c4a5
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-08 06:11:43 -0300

    enable webhook twitter

--- handlers/api.py
+++ handlers/api.py
@@ -12,21 +12,26 @@ def client_deny(event: pyding.EventCall, client):
 
 @pyding.on("http_request")
 def api_route(event, request: web.Request):
+    output = {}
     match request.path.split("/")[1:] if request.path else "":
-        case ["crc"]:
-            sha256_hash_digest = hmac.new(config.get_user_token().encode("utf-8"), msg=request.query_string['crc_token'].encode("utf-8"), digestmod=hashlib.sha256).digest()
-            out = {"status": 200, "http_message": "OK", "response_token": 'sha256=' + base64.b64encode(sha256_hash_digest).decode("utf-8")}
+        case ["webhooks", "twitter"]:
+            if "crc_token" in request.query_string:
+                sha256_hash_digest = hmac.new(config.get_user_token().encode("utf-8"), msg=request.query_string['crc_token'].encode("utf-8"), digestmod=hashlib.sha256).digest()
+                output = {"response_token": 'sha256=' + base64.b64encode(sha256_hash_digest).decode("utf-8")}
+            out = {"status": 200, "http_message": "OK"}
 
         case ["relay"]:
-            out = {"status": 200, "http_message": "OK", "message": "Relayed."}
-            pyding.call("relay_broadcast", message=request.raw_data)
+            out = {"status": 200, "http_message": "OK"}
+            output = {"relayed": True}
+            
             
         case ["acervo", *extra]:
-            out = {"status": 501, "http_message": "OK", "message": "Not Implemented."}
+            out = {"status": 501, "http_message": "OK"}
        
         case _:
-            out = {"status": 404, "http_message": "Not Found",  "message": "Method not found"}
+            out = {"status": 404, "http_message": "Not Found"}
+
+    pyding.call("relay_broadcast", message=request.raw_data)
     
-    
-    out_parsed = json.dumps(out).encode()
-    return web.Response(out['status'], out['http_message'], {"Server": "jdspace", "Content-Type": "application/json", "Content-Length": len(out_parsed)}, out_parsed)
+    output = json.dumps(output).encode()
+    return web.Response(out['status'], out['http_message'], {"Server": "jdspace", "Content-Type": "application/json", "Content-Length": len(output)}, output)
