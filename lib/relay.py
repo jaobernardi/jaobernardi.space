c828c3a8942ae0a4f398573bf44b39c60565d3c6
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-09 02:50:25 -0300

    Code cleanup and broadcast indirect queue

--- lib/relay.py
+++ lib/relay.py
@@ -3,24 +3,35 @@ import pyding
 import threading
 
 
-class RelayServer(pyding.EventSupport):
+class RelayController(pyding.EventSupport):
     def __init__(self):
+        # Init pyding events
         self.register_events()
         self.connections = []
+        self.running = False
+        self.broadcasting = False
 
     def spin_up(self):
-        while True:            
-            for client, req in self.connections:
-                client.send_data(b"keep-alive")
+        self.running = True
+        while self.running:
+            # Broadcast a keep-alive to prevent connections from timing out
+            self.broadcast(b"{}")
             sleep(10)
+    
     @pyding.on("relay_add")
     def add_connection(self, event, client, request):
         self.connections.append([client, request])
 
     def broadcast(self, message):
+        if self.broadcasting:
+            # Prevent broadcasting data while transmitting data
+            while self.broadcasting:
+                continue
+
+        self.broadcasting = True
         for client, req in self.connections:
             client.send_data(message+b"\n")
-
+        self.broadcasting = False
 
     @pyding.on("relay_broadcast")
     def event_broadcast(self, event, message):
