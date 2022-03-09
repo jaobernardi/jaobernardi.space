36b3e7e493e5b0ef30e7b71970bbe0c27441e200
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-09 03:19:22 -0300

    Remove broadcast blocking

--- lib/relay.py
+++ lib/relay.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import sleep, time
 import pyding
 import logging
 
@@ -9,34 +9,29 @@ class RelayController(pyding.EventSupport):
         self.register_events()
         self.connections = []
         self.running = False
-        self.broadcasting = False
+        self.last_broadcast = 0
 
     def spin_up(self):
-        self.running = True
         while self.running:
-            # Broadcast a keep-alive to prevent connections from timing out
-            self.broadcast(b"{}")
-            sleep(10)
-    
+            if (time()-self.last_broadcast) > 10:
+                self.broadcast(b"{}")
+        return
+
     @pyding.on("relay_add")
     def add_connection(self, event, client, request):
         if [client, request] not in self.connections:
             self.connections.append([client, request])
 
     def broadcast(self, message):
-        if self.broadcasting:
-            # Prevent broadcasting data while transmitting data
-            while self.broadcasting:
-                logging.info(f"Waiting to broadcast {message}")
-                continue
-    
-        self.broadcasting = True
+        self.last_broadcast = time()
+
         for client, req in self.connections:
             try:
                 client.send_data(message+b"\n")
             except Exception as e:
+                self.connections.remove([client, req])
                 logging.info("Failed to send broadcast to "+client.address[0])
-        self.broadcasting = False
+
 
 
     @pyding.on("relay_broadcast")
