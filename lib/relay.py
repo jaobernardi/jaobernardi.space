e41a00b2aad7914aafd15bf0cb198a196d43db9e
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-09 01:15:09 -0300

    Drop RSA support

--- lib/relay.py
+++ lib/relay.py
@@ -1,10 +1,5 @@
 from .web import Server
 import pyding
-from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import padding
-from cryptography.hazmat.backends.openssl import backend
-
 import threading
 
 
@@ -29,36 +24,20 @@ class RelayClient:
 
 
 class RelayServer(pyding.EventSupport, Server):
-    def __init__(self, host: str, port: int, private_key: str):
+    def __init__(self, host: str, port: int):
         self.host = host
         self.port = port
         self.running = False
         self.register_events()
         self.connections = []
-        with open(private_key, "rb") as key_file:
-            self.private_key = serialization.load_pem_private_key(
-                key_file.read(),
-                password=None,
-            )
-        self.public_key = self.private_key.public_key()
 
     def handle_connection(self, connection, address):
         client = RelayClient(connection, address)
         self.connections.append(client)
         threading.Thread(target=client.read_data).start()
-        connection.send(f"Relay@{self.port}#{address}".encode("utf-8"))
 
 
     def broadcast(self, message):
-        message = self.private_key.public_key().encrypt(
-            message,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
-            )
-        )
-
         for client in [i for i in self.connections]:
             try:
                 client.send_data(message)
