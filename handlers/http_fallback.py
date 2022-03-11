3ddf2935b4aed3b83048d562ece54833de7f8c2b
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-11 06:24:20 -0300

    Add http fallback

---
+++ handlers/http_fallback.py
@@ -0,0 +1,12 @@
+import pyding
+from lib import web, config, html_parsing
+
+
+@pyding.on("http_request", priority=float("-inf"))
+def fallback_route(event, request: web.Request):
+    if not event.response:
+        fallback_file = open("www/fallback.html", "rb")
+        fallback_page = html_parsing.eval_document(fallback_file.read(), {"request": request})
+        fallback_file.close()
+
+        return web.Response(200, "OK", headers={"Server": "jdspace", "Content-Type": "text/html", "Content-Length": len(fallback_page)}, data=fallback_page)
