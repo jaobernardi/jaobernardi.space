ab76422dd565cca6da2e477b904486c2af3e70a3
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-11 06:50:10 -0300

    Add jdspace image serving

--- handlers/html_serving.py
+++ handlers/html_serving.py
@@ -7,4 +7,14 @@ from os import path
 def html_route(event, request: web.Request):
     if "Host" not in request.headers or request.headers["Host"] != "jaobernardi.space":
         return
-    
+    if request.path == "/jdspace.png":
+        jdspace_logo = open("assets/jdspace.png", "rb")
+        jdspace_logo = jdspace_logo.read()
+        return web.Response(
+            200,
+            "OK",
+            {"Server": "jdspace",
+            "Content-Type": "image/png",
+            "Content-Length": len(jdspace_logo)},
+        jdspace_logo
+        )
