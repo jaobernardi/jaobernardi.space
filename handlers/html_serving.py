cd2df02977e09a7b38b87a5df0ac017232019269
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-11 06:24:12 -0300

    Add html serving

---
+++ handlers/html_serving.py
@@ -0,0 +1,10 @@
+import pyding
+from lib import web, config
+from os import path
+
+
+@pyding.on("http_request", priority=float("inf"))
+def html_route(event, request: web.Request):
+    if "Host" not in request.headers or request.headers["Host"] != "jaobernardi.space":
+        return
+    
