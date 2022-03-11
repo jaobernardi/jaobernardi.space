dc3473a083998eb4430f58b0a04aa01ce5a3b122
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-10 22:37:46 -0300

    Checking twitter signature

--- handlers/api.py
+++ handlers/api.py
@@ -15,18 +15,8 @@ def add_connection(client, request):
 @pyding.on("http_request")
 def api_route(event, request: web.Request):
     output = {}
-    match request.path.split("/")[1:] if request.path else "":
-        case ["webhooks", "twitter"]:
-            # Verify Twitter Headers
-            print(request.raw_data)
-        
-            sha256_hash_digest = hmac.new(
-                config.get_user_token().encode("utf-8"),
-                msg=request.data,
-                digestmod=hashlib.sha256)\
-                .digest()
-            print(base64.b64encode(sha256_hash_digest).decode("utf-8"))
-
+    match request.method, request.path.split("/")[1:] if request.path else "", request.headers:
+        case "GET", ["webhooks", "twitter"], headers:
             # Do the CRC challange for twitter
             if "crc_token" in request.query_string:
                 # Do the hash things
@@ -39,27 +29,37 @@ def api_route(event, request: web.Request):
                 output = {
                     "response_token": crc
                 }
-                print(output)
-            # Return 200 OK
-            http_status = {"status": 200, "message": "OK"}
-            # Relay data
-            if request.data:
-                pyding.call("relay_broadcast", message=request.data)
+        case "POST", ["webhooks", "twitter"], {"X-Twitter-Webhooks-Signature": twitter_signature, **headers}:
+            # Check twitter headers
+            sha256_hash_digest = hmac.new(
+                config.get_user_token().encode("utf-8"),
+                msg=request.data,
+                digestmod=hashlib.sha256)\
+                .digest()
+            
+            signature = b'sha256=' + base64.b64encode(sha256_hash_digest)
+            print(signature, twitter_signature)
+            if signature == twitter_signature:
+                http_status = {"status": 200, "message": "OK"}
+                # Relay data
+                if request.data:
+                    pyding.call("relay_broadcast", message=request.data)
+            else:
+                http_status = {"status": 403, "message": "Forbidden"}
 
-        case ["webhooks", "twitter", "stream"]:
+        case method, ["webhooks", "twitter", "stream"], headers:
             # Return a response with a handover handler function.
             return web.Response(200, "OK", {"Server": "jdspace"}, add_connection)
 
-        case ["relay"]:
+        case "POST", ["relay"], headers:
             http_status = {"status": 200, "message": "OK"}
             output = {"relayed": True}
-            
-            
-        case ["acervo", *extra]:
+             
+        case "POST", ["acervo", *extra], headers:
             http_status = {"status": 501, "message": "OK"}
-       
+
         case _:
-            http_status = {"status": 404, "message": "Not Found"}
+            http_status = {"status": 403, "message": "Forbidden"}
 
     
     
