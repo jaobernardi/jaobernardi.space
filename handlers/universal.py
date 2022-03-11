fb35085c2e0effb05f10c20ba2cd8148c2f5f27c
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-11 06:39:11 -0300

    Fix path issue

--- handlers/universal.py
+++ handlers/universal.py
@@ -4,7 +4,7 @@ from lib import web, config, html_parsing
 
 @pyding.on("http_request", priority=float("inf"))
 def universal_files(event, request: web.Request):
-    if request.path == "robots.txt":
+    if request.path == "/robots.txt":
         robots = open("www/robots.txt", "rb")
         robots = robots.read()
         return web.Response(
