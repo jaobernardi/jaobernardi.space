d73526b6f8b2058bb22baff571635880379ad78f
Jão Bernardi <joao_bernardi@outlook.com>
2022-03-16 19:31:39 -0300

    Return 503 for Fallback

--- handlers/http_fallback.py
+++ handlers/http_fallback.py
@@ -9,4 +9,4 @@ def fallback_route(event, request: web.Request, client: web.Client):
         fallback_page = html_parsing.eval_document(fallback_file.read(), {"request": request})
         fallback_file.close()
 
-        return web.Response(200, "OK", headers={"Server": "jdspace", "Content-Type": "text/html", "Content-Length": len(fallback_page)}, data=fallback_page)
+        return web.Response(503, "Service Unavailable", headers={"Server": "jdspace", "Content-Type": "text/html", "Content-Length": len(fallback_page)}, data=fallback_page)
