6da646bfacfcd32d69f3996a7049f2af6d5c3236
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-22 23:12:57 -0300

    Add alias

--- handlers/file_serving.py
+++ handlers/file_serving.py
@@ -47,27 +47,36 @@ def html_route(event, request: web.Request, client: web.Client):
         not_found_file = html_parsing.eval_document(not_found_file, {"request": request})
         return web.Response(404, "Not Found", headers | {"Content-Type": "text/html", "Content-Length": len(not_found_file)}, not_found_file)
     
-    if os.path.dirname(path) == ".settings":
+    if os.path.basename(path) == ".settings":
         return web.Response(403, "Forbidden", headers | {"Content-Type": "text/html", "Content-Length": len(forbidden_file)}, forbidden_file)
 
 
-    if ".settings" in os.listdir(os.path.dirname(path)):
+    if ".settings" in os.listdir(os.path.dirname(path)):        
+        filename = os.path.basename(path)
         settings_path = pathlib.Path(os.path.dirname(path)) / pathlib.Path(".settings")
         path_settings = json.load(open(settings_path))
         
-        if "hidden" in path_settings and os.path.basename(path) in path_settings["hidden"]:
+        if "alias" in path_settings and filename in path_settings['alias']:
+            path = os.path.dirname(path)+"/"+path_settings['alias'][filename]
+
+        
+        if "hidden" in path_settings and filename in path_settings["hidden"]:
             forbidden_file = open("assets/generic_403.html", "rb").read()
             forbidden_file = html_parsing.eval_document(forbidden_file, {"request": request})
             return web.Response(403, "Forbidden", headers | {"Content-Type": "text/html", "Content-Length": len(forbidden_file)}, forbidden_file)
 
-
-    found_file = open(str(path), "rb").read()
-    found_file = html_parsing.eval_document(found_file, {"request": request})
-    return web.Response(
-        200,
-        "OK",
-        headers | {
-            "Content-Type": mimetypes.MimeTypes().guess_type(str(path))[0],
-            "Content-Length": len(found_file)
-            },
-        found_file)
+    try:
+        found_file = open(str(path), "rb").read()
+        found_file = html_parsing.eval_document(found_file, {"request": request})
+        return web.Response(
+            200,
+            "OK",
+            headers | {
+                "Content-Type": mimetypes.MimeTypes().guess_type(str(path))[0],
+                "Content-Length": len(found_file)
+                },
+            found_file)
+    except:
+        internal_error = open("assets/generic_500.html", "rb").read()
+        internal_error = html_parsing.eval_document(internal_error, {"request": request})
+        return web.Response(500, "Internal Server Error", headers | {"Content-Type": "text/html", "Content-Length": len(internal_error)}, internal_error)
