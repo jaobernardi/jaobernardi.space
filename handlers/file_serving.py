afd0936ccf0d539b0937a8a8bef85657637ade1c
Jão Bernardi <joao_bernardi@outlook.com>
2022-03-23 21:57:46 -0300

    Cleanup 1/2

--- handlers/file_serving.py
+++ handlers/file_serving.py
@@ -9,7 +9,17 @@ import mimetypes
 headers = {"X-Backend": "Content", "Server": "jdspace"}
 
 
-# TODO: FIX & CLEAN THIS AFTER!!!
+# TODO: FIX & CLEAN THIS AFTER!!! ok
+
+
+def load_error(code, **kwargs):
+    if f"generic_{code}.html" in os.listdir("assets"):
+        error_file = open(f"assets/generic_{code}.html", "rb").read()
+    else:
+        error_file = open(f"assets/generic_error.html", "rb").read()
+
+    html_error = html_parsing.eval_document(error_file, kwargs | {"code": code})
+    return html_error, len(html_error)
 
 
 @pyding.on("http_request", priority=float("inf"))
@@ -17,66 +27,49 @@ def html_route(event, request: web.Request, client: web.Client):
     if "Host" not in request.headers or request.headers["Host"] != "jaobernardi.space":
         return
 
-    if request.path.endswith("jdspace.png"):
-        jdspace_logo = open("assets/jdspace.png", "rb")
-        jdspace_logo = jdspace_logo.read()
-        return web.Response(
-            200,
-            "OK",
-            {
-            "Content-Type": "image/png",
-            "Content-Length": len(jdspace_logo)
-            } | headers,
-        jdspace_logo
-        )
-
-    elif request.path.endswith("archive.png"):
-        archive_logo = open("assets/archive.png", "rb")
-        archive_logo = archive_logo.read()
-        return web.Response(
-            200,
-            "OK",
-            {
-                "Content-Type": "image/png",
-                "Content-Length": len(archive_logo)
-            } | headers,
-        archive_logo
-        )
+    # Serve files
     
     path = pathlib.Path("www") / pathlib.Path(request.path.removeprefix("/"))
-    if path.is_dir():
-        path = path / pathlib.Path("index.html")
     
-    if os.path.basename(path) == ".settings":
-        return web.Response(403, "Forbidden", headers | {"Content-Type": "text/html", "Content-Length": len(forbidden_file)}, forbidden_file)
+    # Check if file is a path
+    if path.is_dir():
+        # Set file to index.html
+        path = path / pathlib.Path("index.html")
+
+    filename = os.path.basename(path)
+    dirname = os.path.dirname(path)
+
+    # Check if requested file is .settings (403 Forbidden)
+    if filename == ".settings":
+        error_html, error_size = load_error(403, request=request)
+        return web.Response(403, "Forbidden", headers | {"Content-Type": "text/html", "Content-Length": error_size}, error_html)
 
 
-    if ".settings" in os.listdir(os.path.dirname(path)):        
-        filename = os.path.basename(path)
+    # Check if there is a '.settings' file in path.
+    if ".settings" in os.listdir(dirname):   
+        
         settings_path = pathlib.Path(os.path.dirname(path)) / pathlib.Path(".settings")
         path_settings = json.load(open(settings_path))
         
+        # Check if filename has an alias
         if "alias" in path_settings and filename in path_settings['alias']:
-            path = os.path.dirname(path)+"/"+path_settings['alias'][filename]
-            path = pathlib.Path(path)
+            path = path.parent / pathlib.Path(path_settings['alias'][filename])
             filename = path_settings['alias'][filename]
         
+        # Check if filename is hidden (will return 403 Forbidden)
         if "hidden" in path_settings and filename in path_settings["hidden"]:
-            forbidden_file = open("assets/generic_403.html", "rb").read()
-            forbidden_file = html_parsing.eval_document(forbidden_file, {"request": request})
-            return web.Response(403, "Forbidden", headers | {"Content-Type": "text/html", "Content-Length": len(forbidden_file)}, forbidden_file)
+            error_html, error_size = load_error(403, request=request)
+            return web.Response(403, "Forbidden", headers | {"Content-Type": "text/html", "Content-Length": error_size}, error_html)
 
         if "require_auth" in path_settings and filename in path_settings["require_auth"]:
             cred = base64.b64encode(config.get_stream_auth().encode("utf-8")).decode("utf-8")
             if "Authorization" not in request.headers or f'Basic {cred}' == request.headers['Authorization']:
-                forbidden_file = open("assets/generic_403.html", "rb").read()
-                forbidden_file = html_parsing.eval_document(forbidden_file, {"request": request})
-                return web.Response(401, "Unauthorized", headers | {"WWW-Authenticate": f"Basic realm=\"{path_settings['require_auth'][filename]}\"", "Content-Type": "text/html", "Content-Length": len(forbidden_file)}, forbidden_file)
+                error_html, error_size = load_error(401, request=request)
+                return web.Response(401, "Unauthorized", headers | {"WWW-Authenticate": f"Basic realm=\"{path_settings['require_auth'][filename]}\"", "Content-Type": "text/html", "Content-Length": error_size}, error_html)
 
     if not path.exists():
-        not_found_file = open("assets/generic_404.html", "rb").read()
-        not_found_file = html_parsing.eval_document(not_found_file, {"request": request})
-        return web.Response(404, "Not Found", headers | {"Content-Type": "text/html", "Content-Length": len(not_found_file)}, not_found_file)
+        error_html, error_size = load_error(404, request=request)
+        return web.Response(404, "Not Found", headers | {"Content-Type": "text/html", "Content-Length": error_size}, error_html)
     
     try:
         found_file = open(str(path), "rb").read()
@@ -90,6 +83,5 @@ def html_route(event, request: web.Request, client: web.Client):
                 },
             found_file)
     except:
-        internal_error = open("assets/generic_500.html", "rb").read()
-        internal_error = html_parsing.eval_document(internal_error, {"request": request})
-        return web.Response(500, "Internal Server Error", headers | {"Content-Type": "text/html", "Content-Length": len(internal_error)}, internal_error)
+        error_html, error_size = load_error(500, request=request)
+        return web.Response(500, "Internal Server Error", headers | {"Content-Type": "text/html", "Content-Length": error_size}, error_html)
