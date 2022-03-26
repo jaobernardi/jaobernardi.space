0d016de525fe6b3c757d2c4115159990069d5153
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-26 16:45:40 -0300

    Support url in twitter/video/

--- handlers/services.py
+++ handlers/services.py
@@ -1,8 +1,17 @@
+from email.quoprimime import unquote
 import pyding
 import requests
 from lib import web, config, twitter
 import logging
-from urllib.parse import unquote
+from urllib.parse import unquote, urlparse
+from random import choices
+from string import ascii_letters
+
+
+def relay_data(url, chunksize=1024):
+    req = requests.get(url, stream=True)
+    for data in req.iter_content(chunksize):
+        yield data
 
 
 @pyding.on("http_request")
@@ -12,24 +21,47 @@ def services_route(event, request: web.Request, client: web.Client):
         return
 
     match request.method, request.path.split("/")[1:] if request.path else "", request.headers:
-        case method, ["twitter", "video", id, *extra], head:
-            def send_data(url, chunksize=1024):
-                req = requests.get(video_url, stream=True)
-                for data in req.iter_content(chunksize):
-                    yield data
-        
+        case "GET", ["twitter", "video", id]:
+                http_status = {
+                    "status": 301,
+                    "message": "Permanent Redirect",
+                    "headers": {
+                        "Location": f"/twitter/video/id/{id}"
+                    }
+                }
 
-            try:
-                chunksize = 1024
+        case "GET", ["twitter", "video", "url", url], head:
+            url = unquote(url)
+            parsed_url = urlparse(url)
+            if parsed_url.netloc == "video.twimg.com":
+                filename = "".join(choices(ascii_letters, k=16))
+                http_status = {
+                    "status": 200,
+                    "message": "Ok",
+                    "headers": {
+                        "Content-Type": "video/mp4",
+                        "Content-Disposition": f"attachment; filename=\"{filename}.mp4\"",
+                    }
+                }
+
+                output = relay_data(url)
+            else:
+                failed = open("assets/generic_403.html", "rb")
+                output = failed.read()
+                failed.close()
+                http_status = {
+                    "status": 403,
+                    "message": "Forbidden",
+                    "headers": {
+                        "Content-Type": "text/html",
+                        "Content-Length": len(output),
+                    }
+                }
             
-                if "chunksize" in request.query_string:
-                    chunksize = int(request.query_string['chunksize'])
-                    if chunksize > 1024:
-                        chunksize = 1024
-                print(id)
-                video_url = twitter.get_video(id) if "is_url" not in request.query_string else unquote(id)
+        case "GET", ["twitter", "video", "id", id], head:        
+            try:
+                video_url = twitter.get_video(id)
                 
-
                 http_status = {
                     "status": 200,
                     "message": "Ok",
@@ -39,7 +71,7 @@ def services_route(event, request: web.Request, client: web.Client):
                     }
                 }
 
-                output = send_data(video_url, chunksize)
+                output = relay_data(video_url)
             except:
                 failed = open("assets/services_fail.html", "rb")
                 output = failed.read()
