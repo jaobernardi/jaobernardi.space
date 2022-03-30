0721ca979666e67b3b17b0ea219605372b295140
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-03-30 18:08:52 -0300

    Add auth handler

--- handlers/auth.py
+++ handlers/auth.py
@@ -5,4 +5,6 @@ import logging
 
 @pyding.on("http_request")
 def auth_handler(event, request: web.Request, client: web.Client):
+    if "Host" not in request.headers or request.headers["Host"] != "auth.jaobernardi.space":
+        return
     return web.Response(204, "No Content", {"X-Backend": "Auth"})
