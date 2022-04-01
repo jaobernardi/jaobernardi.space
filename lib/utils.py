bada1f3b35f200b7425bb5009b7513c917894603
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-04-01 04:20:14 -0300

    Add timeout list

--- lib/utils.py
+++ lib/utils.py
@@ -8,6 +8,33 @@ def load_handlers():
             __import__("handlers."+handler.removesuffix(".py"))
 
 
+class TimeoutList:
+    def __init__(self, ttl):
+        self._store = {}
+        self.ttl = ttl
+    
+    def check(self):
+        for item in list(self._store):
+            if self._store[item]['expiry'] <= time.time():
+                self._store.pop(item)
+
+    def append(self, item):
+        self.check()
+        self._store[item] = {"expiry": time.time()+self.ttl}    
+
+    def __contains__(self, item):
+        self.check()
+        return item in self._store
+
+    def __repr__(self):
+        self.check()
+        return list(self._store).__repr__()
+
+    def __iter__(self):
+        self.check()
+        return self._store.__iter__()
+
+
 class TimeoutDict:
     def __init__(self, ttl):
         self._store = {}
