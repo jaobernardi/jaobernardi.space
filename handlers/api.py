91e73fb2736a59cd15230758e54d05f9616dffc6
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-04-01 04:00:30 -0300

    Send host along with http_request event

--- handlers/api.py
+++ handlers/api.py
@@ -14,7 +14,7 @@ def handover_connection(client, request):
 
 
 @pyding.on("http_request")
-def api_route(event, request: web.Request, client: web.Client):
+def api_route(event, request: web.Request, client: web.Client, host: str):
     
     if "Host" not in request.headers or request.headers["Host"] != "api.jaobernardi.space":
         return
