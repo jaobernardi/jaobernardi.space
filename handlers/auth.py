10d8c0fd84c609900180eccb5027f80176b10d2a
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-04-01 04:14:38 -0300

    Spotify auth

--- handlers/auth.py
+++ handlers/auth.py
@@ -6,19 +6,26 @@ import logging
 
 @pyding.on("http_request", host="auth.jaobernardi.space")
 def auth_handler(event, request: web.Request, client: web.Client, host: str):
-    match request.path.split("/")[1:]:
-        case ["spotify", "authenticate"]:            
-            return web.Response(
-                "301",
-                "Moved Permanently",
-                {
-                    "X-Backend": "Auth",
-                    "Location": f"https://accounts.spotify.com/authorize?response_type=code&client_id={config.get_spotify_client_id()}&scope=user-modify-playback-state user-read-currently-playing user-read-playback-state streaming&redirect_uri=https://auth.jaobernardi.space/spotify/callback"
-                    
-                }
+    match request.method, request.path.split("/")[1:], request.query_string:
+        case "GET", ["spotify", "authenticate"], _params:            
+            return web.Response.redirect(
+                f"https://accounts.spotify.com/authorize?response_type=code&client_id={config.get_spotify_client_id()}&scope=user-modify-playback-state user-read-currently-playing user-read-playback-state streaming&redirect_uri=https://auth.jaobernardi.space/spotify/callback",
+                {"X-Backend": "Auth"}
             )
-        case ["spotify", "callback"]:
-            logging.info(request.query_string)
-            return web.Response(204, "No Content", {"X-Backend": "Auth"})
+
+        case "GET", ["spotify", "callback"], {"code": code, "state": state, **_params}:
+            asset = open(f"assets/spotify/access_success.html", "rb")
+            asset = asset.read()
+            content_type = "text/html"
+
+            return web.Response.ok(asset, content_type, {"X-Backend": "Auth"})
+
+        case "GET", ["spotify", "callback"], _params:
+            asset = open(f"assets/spotify/access_fail.html", "rb")
+            asset = asset.read()
+            content_type = "text/html"
+
+            return web.Response.ok(asset, content_type, {"X-Backend": "Auth"})
+
         case _:
             return web.Response(204, "No Content", {"X-Backend": "Auth"})
