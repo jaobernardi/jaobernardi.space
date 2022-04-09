9679ca485474712b801581625dbedd806c3a0764
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-04-09 03:47:27 -0300

    Make config bettert

--- lib/config.py
+++ lib/config.py
@@ -5,38 +5,12 @@ def get_data():
         data = json.load(file)
     return data
 
-def get_otp():
-    return get_data()['otp']
 
-def get_database():
-    return get_data()['database']
+def __getattr__(name: str):
+    if name.startswith("get_"):
+        item = name.removeprefix("get_")
+        data = get_data()
+        return lambda: data[item] if item in data else None
 
-def get_private_key():
-    return get_data()['private_key']
-
-def get_public_key():
-    return get_data()['public_key']
-
-def get_https_redirect():
-    return get_data()['https_redirect']
-
-def get_spotify_client_id():
-    return get_data()['spotify_client_id']
-
-def get_https_redirect_settings():
-    return get_data()['https_redirect_settings']
-
-def get_stream_auth():
-    return get_data()['stream_auth']
-
-def get_user_token():
-    return get_data()['user_token']
-
-def get_web():
-    return get_data()['web']
-
-def get_root():
-    return get_data()['web_root']
-
-def get_rate():
-    return get_data()['connection_rate']
+def __dir__():
+    return [f"get_{i}" for i in get_data().keys()]
