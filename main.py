47dab4290ed7a6e8e2891093be5fdc77b9aff4f0
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-05-13 23:19:49 -0300

    Rewrite main

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-from lib import web
-import pyding
+from lib.structures import HTTPServer, Response
+from lib import config, utils
 
+utils.load_handlers()
 
-# Handle http requests
-@pyding.on("http_request")
-def responder(event, request):
-    # Always return 200 OK
-    return web.Response(200, "OK", data=request.data)
-
-
-def main():
-    # Setup server
-    server = web.Server("127.0.0.1", 1220)
-    server.spin_up()
-
-
-if __name__ == "__main__":
-    main()
-
+server = HTTPServer(**config.get_web())
+server.spin_up()
