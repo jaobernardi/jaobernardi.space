53ad4f54b23cb506f141ac51e3472fba197d70d4
João Lucas Bernardi <joao_bernardi@outlook.com>
2022-05-13 23:17:03 -0300

    Structures: Init

---
+++ lib/structures/__init__.py
@@ -0,0 +1,6 @@
+from .user import User
+from .client import Client
+from .response import Response
+from .request import Request
+from .server import Server
+from .http import HTTPRedirecterServer, HTTPServer
